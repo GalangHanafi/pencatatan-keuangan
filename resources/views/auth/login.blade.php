5e2ef6d0b9234f35a125910ad198866a1f614e3c
novahardiko <79013576+novahardiko@users.noreply.github.com>
2024-05-22 18:55:25 +0700

    perbaiki login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,26 +27,28 @@
                 <form action="{{route('login')}}" method="POST">
                   @csrf
                   <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <label for="exampleInputEmail1" class="form-label">Email</label>
                     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
                   </div>
                   <div class="mb-4">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
                   </div>
                   <div class="d-flex align-items-center justify-content-between mb-4">
                     <div class="form-check">
-                      <input class="form-check-input primary" type="checkbox" value="" id="flexCheckChecked" checked>
-                      <label class="form-check-label text-dark" for="flexCheckChecked">
+                      <input name="remember" class="form-check-input primary"  type="checkbox" value="" id="flexCheckChecked" checked>
+                      <label class="form-check-label text-dark"  for="flexCheckChecked">
                         Remeber this Device
                       </label>
                     </div>
-                    <a class="text-primary fw-bold" href="./index.html">Forgot Password ?</a>
+                    <a class="text-primary fw-bold" href="{{route('password.request')}}">Forgot Password ?</a>
                   </div>
                   <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Sign In</button>
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">New to Modernize?</p>
-                    <a class="text-primary fw-bold ms-2" href="./authentication-register.html">Create an account</a>
+                    <a class="text-primary fw-bold ms-2" href="{{route('register')}}">Create an account</a>
                   </div>
                 </form>
               </div>
