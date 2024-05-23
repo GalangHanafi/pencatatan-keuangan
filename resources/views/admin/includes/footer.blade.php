1f3d3c977bbc632a681b72476cdceb8601dc4e2e
GalangHanafi <galanghanafi9131@gmail.com>
2024-05-23 10:52:10 +0700

    menambahkan laman category(create, edit, index), perbaiki include(head, navbar, sidebar, footer) nambahin fungsi asset

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -32,13 +32,13 @@
 
 
 
-<script src="admin/src/assets/libs/jquery/dist/jquery.min.js"></script>
-<script src="admin/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="admin/src/assets/js/sidebarmenu.js"></script>
-<script src="admin/src/assets/js/app.min.js"></script>
-<script src="admin/src/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-<script src="admin/src/assets/libs/simplebar/dist/simplebar.js"></script>
-<script src="admin/src/assets/js/dashboard.js"></script>
+<script src="{{ asset('admin/src/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('admin/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('admin/src/assets/js/sidebarmenu.js') }}"></script>
+<script src="{{ asset('admin/src/assets/js/app.min.js') }}"></script>
+<script src="{{ asset('admin/src/assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+<script src="{{ asset('admin/src/assets/libs/simplebar/dist/simplebar.js') }}"></script>
+<script src="{{ asset('admin/src/assets/js/dashboard.js') }}"></script>
 </body>
 
 </html>
