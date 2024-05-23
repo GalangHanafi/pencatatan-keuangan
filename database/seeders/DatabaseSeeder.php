80b2bf9e89b9972cbee5adf594ea9574f303ee98
GalangHanafi <galanghanafi9131@gmail.com>
2024-05-23 10:14:48 +0700

    perbaiki remember me di login page, nambah DefaultCategorySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,16 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         $this->call([
             User1Seeder::class,
             User2Seeder::class,
+            DefaultCategorySeeder::class,
         ]);
     }
 }
