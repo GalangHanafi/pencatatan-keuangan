c7db39fd230ad17432f9d7de97e1c96a9c265c69
GalangHanafi <galanghanafi9131@gmail.com>
2024-05-28 17:15:08 +0700

    menambahkan link di logo dashboard

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Sidebar scroll-->
     <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="index.html" class="text-nowrap logo-img">
+            <a href="{{ route('dashboard') }}" class="text-nowrap logo-img">
                 <img src="{{ asset('admin/src/assets/images/logos/kantongku.png') }}" width="180" alt="" />
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
