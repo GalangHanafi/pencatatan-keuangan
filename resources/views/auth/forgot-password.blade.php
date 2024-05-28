22ddf1753b60d3f66cd313429ae05b803652e2d6
pc-rumah <wow23787@gmail.com>
2024-05-28 09:06:49 +0700

    custom forgot pw dan verify pw

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,49 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('admin/src/assets/images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/src/assets/css/styles.min.css') }}" />
+</head>
+<body>
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div
+            class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-8 col-lg-6 col-xxl-3">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                    <img src="{{ asset('admin/src/assets/images/logos/dark-logo.svg') }}" width="180"
+                                        alt="">
+                                </a>
+                                <p class="text-center">Your Social Campaigns</p>
+          <form method="post" action="{{route('password.email')}}" class="card-body px-5">
+            @csrf
+            <p class="card-text py-2">
+              Enter your email address and we'll send you an email with instructions to reset your password.
+            </p>
+            <div data-mdb-input-init class="form-outline">
+              <label class="form-label" for="typeEmail">Email input</label>
+              <input type="email" id="typeEmail" name="email" class="form-control my-3" />
+              <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+            <button type="submit" data-mdb-ripple-init class="btn btn-primary w-100">Reset password</button>
+        </form>
+        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <script src="{{ asset('admin/src/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+</body>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,34 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+<!doctype html>
+<html lang="en">
 
-    @if (session('status') == 'verification-link-sent')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modernize Free</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('admin/src/assets/images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/src/assets/css/styles.min.css') }}" />
+</head>
+<body>
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <div
+            class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="d-flex align-items-center justify-content-center w-100">
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-8 col-lg-6 col-xxl-3">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                                    <img src="{{ asset('admin/src/assets/images/logos/dark-logo.svg') }}" width="180"
+                                        alt="">
+                                </a>
+                                <p class="text-center">Your Social Campaigns</p>
+                                <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        <p class="card-text">Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you?</p>
+
+        @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            <h5>A new verification link has been sent to the email address you provided during registration.</h5>
         </div>
     @endif
 
@@ -14,18 +37,30 @@
             @csrf
 
             <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                <button class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2" type="submit">Resend Verification Email</button>
             </div>
         </form>
 
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
+            <button type="submit" class="btn btn-link position-fixed ">
+                Log Out
             </button>
+            
+            
         </form>
     </div>
-</x-guest-layout>
+    </div>
+        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('admin/src/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+
+</html>
