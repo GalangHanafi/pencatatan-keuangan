b7cec4bc4ab871c359e5fc4424e02de551a00419
GalangHanafi <galanghanafi9131@gmail.com>
2024-05-30 12:56:57 +0700

    rapi rapi

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -38,8 +38,7 @@
                     <x-primary-button>{{ __('Save') }}</x-primary-button>
 
                     @if (session('status') === 'password-updated')
-                        <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                            class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
+                        <p class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
                     @endif
                 </div>
             </form>
