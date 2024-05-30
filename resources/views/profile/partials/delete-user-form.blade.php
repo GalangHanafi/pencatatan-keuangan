2a1e26e2fcec0354710f2ec98d5084be07a9dbb2
GalangHanafi <galanghanafi9131@gmail.com>
2024-05-30 12:54:38 +0700

    rapi rapi edit profile

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -2,68 +2,52 @@
     <div class="card">
         <div class="card-body">
             <h2 class="card-title fw-semibold mb-4">
-                Delete Account
+                Delete User
             </h2>
-    
+
             <p>
-                Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain
+                Once your user is deleted, all of its resources and data will be permanently deleted. Before deleting
+                your user, please download any data or information that you wish to retain
             </p>
             <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">{{ __('Delete Account') }}</button>
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">{{ __('Delete User') }}</button>
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
-                    @csrf
-                    @method('delete')
-        
-                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                        {{ __('Are you sure you want to delete your account?') }}
-                    </h2>
-        
-                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                        {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-                    </p>
-        
-                    {{-- <div class="mt-6">
-                        <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
-        
-                        <x-text-input
-                            id="password"
-                            name="password"
-                            type="password"
-                            class="mt-1 block w-3/4"
-                            placeholder="{{ __('Password') }}"
-                        />
-        
-                        <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-                    </div> --}}
-                    <div class="mb-3">
-                        <label class="form-label" for="password">Confirm Password</label>
-                        <input class="form-control" id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-                        
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Delete Account</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+                            <div class="modal-body">
+                                @csrf
+                                @method('delete')
+
+                                <h2>
+                                    {{ __('Are you sure you want to delete your account?') }}
+                                </h2>
+
+                                <p class="mt-1">
+                                    {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                                </p>
+
+                                <div>
+                                    <label class="form-label" for="password">Confirm Password</label>
+                                    <input class="form-control" id="password" name="password" type="password"
+                                        class="mt-1 block w-full" autocomplete="new-password" />
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-danger">Delete User</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-danger">Delete User</button>
-                      </div>
-                </form>
-              
             </div>
-          </div>
-        
-
-    </div>
-
         </div>
-    </div>
-
-    
 </section>
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -4,7 +4,7 @@
             <h2 class="card-title fw-semibold mb-4">
                 Update Password
             </h2>
-    
+
             <p>
                 Ensure your account is using a long, random password to stay secure
             </p>
@@ -12,36 +12,34 @@
             <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
                 @csrf
                 @method('put')
-        
+
                 <div class="mb-3">
-                    <label class="form-label" for="update_password_current_password" >Current password</label>
-                    <input class="form-control" id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+                    <label class="form-label" for="update_password_current_password">Current password</label>
+                    <input class="form-control" id="update_password_current_password" name="current_password"
+                        type="password" class="mt-1 block w-full" autocomplete="current-password" />
                     <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
                 </div>
-        
+
                 <div class="mb-3">
                     <label class="form-label" for="update_password_password">Password</label>
-                    <input class="form-control" id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                    <input class="form-control" id="update_password_password" name="password" type="password"
+                        class="mt-1 block w-full" autocomplete="new-password" />
                     <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
                 </div>
-        
+
                 <div class="mb-3">
                     <label class="form-label" for="update_password_password_confirmation">Confirm Password</label>
-                    <input class="form-control" id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                    <input class="form-control" id="update_password_password_confirmation" name="password_confirmation"
+                        type="password" class="mt-1 block w-full" autocomplete="new-password" />
                     <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
                 </div>
-        
+
                 <div class="flex items-center gap-4">
                     <x-primary-button>{{ __('Save') }}</x-primary-button>
-        
+
                     @if (session('status') === 'password-updated')
-                        <p
-                            x-data="{ show: true }"
-                            x-show="show"
-                            x-transition
-                            x-init="setTimeout(() => show = false, 2000)"
-                            class="text-sm text-gray-600 dark:text-gray-400"
-                        >{{ __('Saved.') }}</p>
+                        <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                            class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
                     @endif
                 </div>
             </form>
