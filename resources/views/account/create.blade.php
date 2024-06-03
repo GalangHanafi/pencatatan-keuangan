96ba718e0f812ec94af3b6eb17380e19d536d893
pc-rumah <wow23787@gmail.com>
2024-06-03 17:31:45 +0700

    menambah jquery maskmoney

--- resources/views/account/create.blade.php
+++ resources/views/account/create.blade.php
@@ -15,7 +15,7 @@
 
                 <div class="mb-3">
                     <label for="balance" class="form-label">Balance</label>
-                    <input type="number" class="form-control" name="balance" id="balance" required>
+                    <input type="text" class="form-control" name="balance" id="balance" required>
                     <x-input-error :messages="$errors->get('balance')" class="mt-2" />
                 </div>
 
@@ -26,5 +26,21 @@
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
         </form>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.16/dist/jquery.mask.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
+        <script>
+            console.log('js nyambung');
+            $(document).ready(function($){
+                $('#balance').maskMoney({
+                    prefix: 'Rp ',         // Simbol mata uang di depan
+                    allowNegative: false,  // Tidak mengizinkan nilai negatif
+                    thousands: '.',        // Pemisah ribuan
+                    decimal: ',',          // Pemisah desimal
+                    affixesStay: true ,     // Tetap menampilkan prefix
+                    precision: 0,           //angka di belakang ribuan ilang
+                });
+            });
+            </script>
     </div>
 </div>
