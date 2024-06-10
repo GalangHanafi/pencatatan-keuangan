7f84e2fc53bda22ecd14b7e69e66c27af2420dc5
GalangHanafi <galanghanafi9131@gmail.com>
2024-06-10 10:55:39 +0700

    mailtrap test

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware('auth')->group(function () {
 });
 
 // verified
-Route::middleware(['auth',])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::resource('category', CategoryController::class)->except(['show']);
     Route::resource('account', AccountController::class)->except(['show']);
 
