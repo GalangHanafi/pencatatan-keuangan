1e1d58143be0ba11c7d91ad769ed8e79ee7a7f46
GalangHanafi <galanghanafi9131@gmail.com>
2024-06-14 16:03:55 +0700

    perbaiki footer dashboard

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer container-fluid pt-2">
     <div class="row">
         <div class="col-md-4">
-            <h5><i class="ti ti-comet"></i> Tentang Kami</h5>
+            <h5><i class="ti ti-comet"></i>About Us</h5>
             <p>Empower Your Financial Insights With Our Intuitive Accounting System - Simplifying
                 Complexity,
                 Maximizing Efficiency.</p>
@@ -9,7 +9,7 @@
         <div class="col-md-4">
             <h5><i class="ti ti-mailbox"></i> Kontak</h5>
             <ul>
-                <li><i class="ti ti-mail"></i> Email: tes@accountingsystem.com</li>
+                <li><i class="ti ti-mail"></i> Email: kantongku@lankok.com</li>
                 <li><i class="ti ti-phone"></i> Telepon: +62 123 456 7890</li>
             </ul>
         </div>
@@ -26,7 +26,7 @@
     <hr>
     <div class="row">
         <div class="col-12 text-center">
-            <p>&copy; 2024 Accounting System. Semua hak dilindungi.</p>
+            <p>&copy; 2024 Accounting System. All rights reserved.</p>
         </div>
     </div>
 </footer>
