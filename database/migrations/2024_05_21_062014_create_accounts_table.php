dc33f6c2fab6473af27286dba33afa8ad17c3186
GalangHanafi <galanghanafi9131@gmail.com>
2024-06-14 16:41:18 +0700

    hapus soft delete account

--- database/migrations/2024_05_21_062014_create_accounts_table.php
+++ database/migrations/2024_05_21_062014_create_accounts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             // relations with user
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained();
 
             $table->string('name');
             $table->integer('balance')->default(0);
@@ -22,8 +22,6 @@ return new class extends Migration
             $table->boolean('is_default')->default(false);
 
             $table->timestamps();
-            // soft delete
-            $table->softDeletes();
         });
     }
 
