362d3700c90661e6b793f95e21e41c07caef476c
GalangHanafi <galanghanafi9131@gmail.com>
2024-06-24 08:56:37 +0700

    timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('reminder:send-reminder')->everyHour();
+        $schedule->command('reminder:send-reminder')
+            ->everyHour()
+            ->timezone('Asia/Jakarta');
     }
 
     /**
